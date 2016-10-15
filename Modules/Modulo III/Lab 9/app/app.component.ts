c49b6fbc89b903ef0641d9237bed8c89a3dad047
Jorge Emilio Calapiña Rozo <jorgecalapina@gmail.com>
2016-10-15 12:51:58 -0500

    hola mundo
    quemado

--- Modules/Modulo III/Lab 9/app/app.component.ts
+++ Modules/Modulo III/Lab 9/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular App</h1>'
+  template: '<h1>Hola mundo cruel</h1>'
 })
-export class AppComponent { }
+export class AppComponent {
+	
+}
