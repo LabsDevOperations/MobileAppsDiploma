01580d9ffdbd49952def60bca9e6b8cca2df4b80
Jorge Emilio Calapiña Rozo <jorgecalapina@gmail.com>
2016-10-21 19:00:57 -0500

    Lab 10 y 11
    Lab 10 alejandro, Lab 11 yo

---
+++ Modules/Modulo III/Lab 11/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+ export class User {
+ 	id: number;
+ 	name: string;
+ 	address: string;
+ 	phone: number;
+ }
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>{{user.id}}</h1><br />' + 
+  			'<h2>{{user.name}}</h2><br />' +
+  			'<h2>{{user.address}}</h2><br />' +
+  			'<h2>{{user.phone}}</h2><br />' 
+})
+export class AppComponent {
+	user: User = {
+		id: 1,
+		name: "Emilio Calapiña",
+		address: "Av siempre viva",
+		phone: 6859688
+	}
+}
