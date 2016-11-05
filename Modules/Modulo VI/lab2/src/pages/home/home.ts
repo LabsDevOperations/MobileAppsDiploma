2b05d95ef62c256a67dcba0b0797c37ffc15acf4
Jorge Emilio Calapiña Rozo <jorgecalapina@gmail.com>
2016-11-05 12:56:30 -0500

    ionic carga de productos

--- Modules/Modulo VI/lab2/src/pages/home/home.ts
+++ Modules/Modulo VI/lab2/src/pages/home/home.ts
@@ -2,14 +2,50 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+ export class Product { //modelo
+ 	id: number;
+ 	name: string;
+ 	type: string;
+ 	price: number;
+ 	quantity: number;
+ }
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
-  }
+	products: Product[] = PRODUCTS;
+
+	constructor(public navCtrl: NavController) {
+
+	}
 
 }
+
+
+const PRODUCTS: Product[] = [
+    {
+        id: 1,
+        name: "Samsung galaxy 8",
+        type: "smartphone",
+        price: 1500000,
+        quantity: 20
+    },
+    {
+        id: 2,
+        name: "Samsung galaxy 10",
+        type: "smartphone",
+        price: 1500000,
+        quantity: 20
+    },
+    {
+        id: 3,
+        name: "Samsung galaxy 20",
+        type: "smartphone",
+        price: 1500000,
+        quantity: 20
+    }
+];
