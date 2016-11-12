f379f47094bf15b1e085bb685401a3d8f6f76e8d
Jorge Emilio Calapiña Rozo <jorgecalapina@gmail.com>
2016-11-12 08:45:31 -0500

    proyecto en blanco de ionic corrido en emulador

--- Modules/Modulo VI/lab5/src/pages/home/home.ts
+++ Modules/Modulo VI/lab5/src/pages/home/home.ts
@@ -2,14 +2,34 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+export class Product { //modelo
+ 	id: number;
+ 	name: string;
+ 	type: string;
+ 	price: number;
+ 	quantity: number;
+ 	constructor()
+	{
+		this.id = 127,
+		this.name = "Naranja mecánica",
+		this.type = "video",
+		this.price = 50000,
+		this.quantity = 1
+	}
+ }
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
-  }
+	public product = new Product();
+
+	constructor(public navCtrl: NavController) {
+		
+	}
+
+
 
 }
