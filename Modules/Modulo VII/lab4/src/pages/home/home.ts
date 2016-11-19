749d03e7990f6a7ddc8724a0e91094528b61c431
Jorge Emilio Calapiña Rozo <jorgecalapina@gmail.com>
2016-11-19 13:02:49 -0500

    Lab4 Instalacion de plugins
    plugin network
    genymotion
    chrome://inspect
    console

---
+++ Modules/Modulo VII/lab4/src/pages/home/home.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Network } from 'ionic-native';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+	public network: string;
+
+	constructor(public navCtrl: NavController) {
+
+	}
+
+	ngOnInit() {
+	    Network.onConnect().subscribe(() => {
+	      this.network = Network.connection;
+	      setTimeout(() => {
+	        if (Network.connection === 'wifi') {
+	          console.log(Network.connection.toString() + '!!!!!!!!');
+	        } else 
+	        {
+				console.log('!!!!!!!!111111111');
+
+	        }
+
+	      }, 3000);
+	    });
+	}
+}
